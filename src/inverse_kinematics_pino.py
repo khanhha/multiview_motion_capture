4076a52eb96000becb345522fe044750ea34acd7
khanhha <khanhhh89@gmail.com>
2020-11-08 10:58:32 +0100

    bone lenght optimization

--- src/inverse_kinematics_pino.py
+++ src/inverse_kinematics_pino.py
@@ -10,6 +10,8 @@ import mpl_toolkits.mplot3d.axes3d as p3
 from Quaternions import Quaternions
 import cv2
 from util import descendants_mask
+import subprocess
+import time
 from typing import List, Dict, Tuple
 import copy
 from pathlib import Path
@@ -168,24 +170,55 @@ class HumanModel:
         self.data = self.robot.data
         self.model = self.robot.model
 
-        # optimizable joints
-        joint_lists = {"LShoulderYaw": KpsType.L_Shoulder, "RShoulderYaw": KpsType.R_Shoulder,
-                       "LElbowRoll": KpsType.L_Elbow, "RElbowRoll": KpsType.R_Elbow,
-                       "LWristPitch": KpsType.L_Wrist, 'RWristPitch': KpsType.R_Wrist,
-                       "LKneePitch": KpsType.L_Knee, "RKneePitch": KpsType.R_Knee,
-                       "LAnkleRoll": KpsType.L_Ankle, "RAnkleRoll": KpsType.R_Ankle,
-                       "RHipYaw": KpsType.R_Hip, "LHipYaw": KpsType.L_Hip}
+        # joints list for getting forward location for IK optimization
+        joint_lists = {
+            "LShoulderYaw": KpsType.L_Shoulder, "RShoulderYaw": KpsType.R_Shoulder,
+            "LElbowRoll": KpsType.L_Elbow, "RElbowRoll": KpsType.R_Elbow,
+            "LWristPitch": KpsType.L_Wrist, 'RWristPitch': KpsType.R_Wrist,
+            "RHipYaw": KpsType.R_Hip, "LHipYaw": KpsType.L_Hip,
+            "LKneePitch": KpsType.L_Knee, "RKneePitch": KpsType.R_Knee,
+            "LAnkleRoll": KpsType.L_Ankle, "RAnkleRoll": KpsType.R_Ankle,
+        }
+
+        # joints whose rest placements will be optimized.
+        blens_joint_lists = {
+            "LShoulderPitch": KpsType.L_Shoulder, "RShoulderPitch": KpsType.R_Shoulder,
+            "LElbowYaw": KpsType.L_Elbow, "RElbowYaw": KpsType.R_Elbow,
+            "LWristRoll": KpsType.L_Wrist, "RWristRoll": KpsType.R_Wrist,
+            "LHipYaw": KpsType.L_Hip, "RHipYaw": KpsType.R_Hip,
+            "LKneePitch": KpsType.L_Knee, "RKneePitch": KpsType.R_Knee,
+            "LAnklePitch": KpsType.L_Ankle, "RAnklePitch": KpsType.R_Ankle,
+        }
+
         self.target_joint_pino_ids = []
         self.target_joint_types = []
+
+        self.opt_rest_joint_ids = []
+        self.opt_rest_joint_types = []
+        self.rest_bone_dirs = []
+
         for joint in self.model.joints:
-            j_id = joint.id
             if joint.idx_q <= 0:
                 continue
+            j_id = joint.id
             j_name = self.model.names[joint.id]
             # print(j_name, self.model.names[self.model.parents[j_idx]], self.model.joints[j_idx].nq)
             if j_name in joint_lists:
                 self.target_joint_pino_ids.append(j_id)
                 self.target_joint_types.append(joint_lists[j_name])
+
+            if j_name in blens_joint_lists:
+                self.opt_rest_joint_ids.append(j_id)
+                self.opt_rest_joint_types.append(blens_joint_lists[j_name])
+
+                plc = self.model.jointPlacements[j_id]
+                par_plc = self.model.jointPlacements[self.model.parents[j_id]]
+                bdir = par_plc.translation - plc.translation
+                bdir = bdir / np.linalg.norm(bdir) if np.linalg.norm(bdir) > 0.0 else bdir
+                self.rest_bone_dirs.append(bdir)
+
+        assert len(self.target_joint_pino_ids) == len(joint_lists)
+        assert len(self.opt_rest_joint_types) == len(blens_joint_lists)
 
     def get_kps_idx_map(self) -> Dict[KpsType, int]:
         return {kps_type: idx for idx, kps_type in enumerate(self.target_joint_types)}
@@ -225,33 +258,46 @@ class Skeleton:
         return np.array([side_blens[idx] for idx in self.ref_side_to_full_bone_lens_map])
 
 
-def solve_pose_reprojection(skel: HumanModel,
-                            obs_cam_pose_2d: List[np.ndarray],
-                            cam_calibs: List[Calib],
-                            obs_pose_3d: np.ndarray,
-                            obs_kps_idxs: List[int],
-                            skel_kps_idxs: List[int],
-                            init_param: PoseShapeParam,
-                            n_max_iter=5) -> PoseShapeParam:
-    import subprocess
-    import time
-    subprocess.Popen(['gepetto-gui'])
-    time.sleep(4)
+def image_jacobian(P: np.ndarray, proj_homo: np.ndarray):
+    """
+    :param P: 3x4 projection matrix
+    :param proj_homo: result of Px[x,y,z,1]
+    :return:
+    """
+    u, v, w = proj_homo
+    w2 = w * w
+    proj_jac = [[(P[0, 0] * w - P[2, 0] * u) / w2, (P[0, 1] * w - P[2, 1] * u) / w2,
+                 (P[0, 2] * w - P[2, 2] * u) / w2],
+                [(P[1, 0] * w - P[2, 0] * v) / w2, (P[1, 1] * w - P[2, 1] * v) / w2,
+                 (P[1, 2] * w - P[2, 2] * v) / w2]]
+    proj_jac = np.array(proj_jac)
+    return proj_jac
 
-    skel.robot.initDisplay(loadModel=True)
-    skel.robot.display(init_param.pose)
-    skel.robot.viewer.gui.refresh()
 
-    coco_kps_types = get_kps_order(KpsFormat.COCO)
-    for tar_idx in obs_kps_idxs:
-        goal = pin.SE3.Identity()
-        goal.translation = obs_pose_3d[tar_idx, :3]
-        gname = f'world/goal_{coco_kps_types[tar_idx].name}'
-        skel.robot.viewer.gui.addXYZaxis(gname, [1., 0., 0., 1.], .015, 0.1)
-        skel.robot.viewer.gui.applyConfiguration(gname, se3ToXYZQUAT(goal))
+def solve_bone_length_from_2d(skel: HumanModel,
+                              obs_cam_pose_2d: List[np.ndarray],
+                              cam_calibs: List[Calib],
+                              obs_pose_3d: np.ndarray,
+                              obs_kps_idxs: List[int],
+                              skel_kps_idxs: List[int],
+                              init_param: PoseShapeParam,
+                              vis: bool,
+                              n_max_iter=5) -> PoseShapeParam:
+    opt_rest_joint_ids = skel.opt_rest_joint_ids
+    rest_bone_dirs = skel.rest_bone_dirs
+
+    if vis:
+        skel.robot.display(init_param.pose)
         skel.robot.viewer.gui.refresh()
 
-    time.sleep(5)
+        coco_kps_types = get_kps_order(KpsFormat.COCO)
+        for tar_idx in obs_kps_idxs:
+            goal = pin.SE3.Identity()
+            goal.translation = obs_pose_3d[tar_idx, :3]
+            gname = f'world/goal_{coco_kps_types[tar_idx].name}'
+            skel.robot.viewer.gui.addXYZaxis(gname, [1., 0., 0., 1.], .015, 0.1)
+            skel.robot.viewer.gui.applyConfiguration(gname, se3ToXYZQUAT(goal))
+            skel.robot.viewer.gui.refresh()
 
     n_views = len(cam_calibs)
     DT = 1e-1  # damping factor
@@ -262,7 +308,103 @@ def solve_pose_reprojection(skel: HumanModel,
     model, data = skel.model, skel.data
     x = init_param.pose.copy()
     pin_joint_ids = [skel.target_joint_pino_ids[idx] for idx in skel_kps_idxs]
-    while cur_iter < 10000:
+    while cur_iter < n_max_iter:
+        cur_iter += 1
+        pin.forwardKinematics(model, data, x)
+
+        nv = len(opt_rest_joint_ids) * 3
+        ATA = np.zeros((nv, nv))
+        ATb = np.zeros((nv,))
+        all_errors = []
+
+        for v_idx in range(n_views):
+            calib = cam_calibs[v_idx]
+            obs_pose = obs_cam_pose_2d[v_idx]
+            obs_pose = obs_pose[obs_kps_idxs, :]
+            p_3ds = np.array([data.oMi[j_id].translation for j_id in pin_joint_ids])
+            p_3ds_homo = np.concatenate([p_3ds, np.ones((len(p_3ds), 1))], axis=-1)
+            p_3ds_cam = (calib.Rt @ p_3ds_homo.T).T
+            p_2ds_homo = (calib.K @ p_3ds_cam.T)
+            p_2ds = (p_2ds_homo[:2] / p_2ds_homo[-1]).T
+            p_2ds_homo = p_2ds_homo.T
+            n_kps = len(pin_joint_ids)
+            for i in range(n_kps):
+                jac = pin.computeJointKinematicRegressor(model, data, pin_joint_ids[i],
+                                                         pin.LOCAL_WORLD_ALIGNED)[:3, :]
+                jac = np.concatenate([jac[:, (i - 1) * 6:(i - 1) * 6 + 3] for i in opt_rest_joint_ids], axis=1)
+
+                kps_2d = p_2ds[i]
+
+                proj_jac = image_jacobian(calib.P, p_2ds_homo[i])
+
+                jac = proj_jac @ jac
+                err = obs_pose[i, :2] - kps_2d
+                err = -err
+                mu = lm_damping * max(1e-3, err.dot(err))
+                ATA += np.dot(jac.T, jac) + mu * np.eye(nv)
+                ATb += np.dot(-err.T, jac)
+                all_errors.append(err)
+
+        prev_cost = cost
+        cost = np.mean(np.concatenate(np.abs(all_errors)))
+        impr = abs(cost - prev_cost) / prev_cost
+        if impr < impr_stop:
+            break
+
+        dxs = -solve_qp(ATA, ATb)
+
+        dxs = 0.1 * dxs
+        for idx, j_id in enumerate(opt_rest_joint_ids):
+            plc = model.jointPlacements[j_id]
+            bdir = rest_bone_dirs[idx]
+            dst = bdir.dot(dxs[idx * 3:(idx + 1) * 3])
+            disp = bdir * dst
+            plc.translation = plc.translation + disp
+
+        if vis:
+            skel.robot.display(x)
+            skel.robot.viewer.gui.refresh()
+            time.sleep(0.1)
+
+    init_param.pose = x
+
+    return init_param
+
+
+def solve_pose_from_2d(skel: HumanModel,
+                       obs_cam_pose_2d: List[np.ndarray],
+                       cam_calibs: List[Calib],
+                       obs_pose_3d: np.ndarray,
+                       obs_kps_idxs: List[int],
+                       skel_kps_idxs: List[int],
+                       init_param: PoseShapeParam,
+                       vis: bool,
+                       n_max_iter=5) -> PoseShapeParam:
+    if vis:
+        skel.robot.display(init_param.pose)
+        skel.robot.viewer.gui.refresh()
+
+        coco_kps_types = get_kps_order(KpsFormat.COCO)
+        for tar_idx in obs_kps_idxs:
+            goal = pin.SE3.Identity()
+            goal.translation = obs_pose_3d[tar_idx, :3]
+            gname = f'world/goal_{coco_kps_types[tar_idx].name}'
+            skel.robot.viewer.gui.addXYZaxis(gname, [1., 0., 0., 1.], .015, 0.1)
+            skel.robot.viewer.gui.applyConfiguration(gname, se3ToXYZQUAT(goal))
+            skel.robot.viewer.gui.refresh()
+
+        # time.sleep(5)
+
+    n_views = len(cam_calibs)
+    DT = 1e-1  # damping factor
+    cur_iter = 0
+    lm_damping = 1e-3  # damping factor
+    impr_stop = 1e-6  # ending condition. relative error changes
+    cost = 1000
+    model, data = skel.model, skel.data
+    x = init_param.pose.copy()
+    pin_joint_ids = [skel.target_joint_pino_ids[idx] for idx in skel_kps_idxs]
+    while cur_iter < n_max_iter:
         cur_iter += 1
         pin.computeJointJacobians(model, data, x)
         ATA = np.zeros((model.nv, model.nv))
@@ -285,17 +427,7 @@ def solve_pose_reprojection(skel: HumanModel,
                 jac = pin.getJointJacobian(model, data, pin_joint_ids[i], pin.LOCAL_WORLD_ALIGNED)[:3, :]
                 kps_2d = p_2ds[i]
 
-                P = calib.P
-                u, v, w = p_2ds_homo[i]
-                w2 = w * w
-                proj_jac = [[(P[0, 0] * w - P[2, 0] * u) / w2, (P[0, 1] * w - P[2, 1] * u) / w2,
-                             (P[0, 2] * w - P[2, 2] * u) / w2],
-                            [(P[1, 0] * w - P[2, 0] * v) / w2, (P[1, 1] * w - P[2, 1] * v) / w2,
-                             (P[1, 2] * w - P[2, 2] * v) / w2]]
-                proj_jac = np.array(proj_jac)
-
-                # test opencv
-                # ret = cv2.projectPoints(p_3ds, rvec, tvec, calib.K, None)
+                proj_jac = image_jacobian(calib.P, p_2ds_homo[i])
 
                 jac = proj_jac @ jac
                 err = obs_pose[i, :2] - kps_2d
@@ -314,12 +446,13 @@ def solve_pose_reprojection(skel: HumanModel,
 
         x = pin.integrate(model, x, -dv * DT)
 
-        skel.robot.display(x)
-        skel.robot.viewer.gui.refresh()
-        time.sleep(0.1)
+        if vis:
+            skel.robot.display(x)
+            skel.robot.viewer.gui.refresh()
+            time.sleep(0.1)
+
     init_param.pose = x
 
-    input()
     return init_param
 
 
@@ -335,11 +468,6 @@ def solve_pose_reprojection_auto(skel: HumanModel,
     subprocess.Popen(['gepetto-gui'])
 
     n_views = len(obs_cam_projs)
-    DT = 1e-1  # damping factor
-    cur_iter = 0
-    lm_damping = 1e-3  # damping factor
-    impr_stop = 1e-6  # ending condition. relative error changes
-    cost = 1000
     model, data = skel.model, skel.data
     x = init_param.pose.copy()
     pin_joint_ids = [skel.target_joint_pino_ids[idx] for idx in skel_kps_idxs]
@@ -382,12 +510,12 @@ def solve_pose_reprojection_auto(skel: HumanModel,
     return init_param
 
 
-def solve_pose(skel: HumanModel,
-               obs_pose_3d: np.ndarray,
-               obs_kps_idxs: List[int],
-               skel_kps_idxs: List[int],
-               init_param: PoseShapeParam,
-               n_max_iter=5) -> PoseShapeParam:
+def solve_pose_from_3d(skel: HumanModel,
+                       obs_pose_3d: np.ndarray,
+                       obs_kps_idxs: List[int],
+                       skel_kps_idxs: List[int],
+                       init_param: PoseShapeParam,
+                       n_max_iter=5) -> PoseShapeParam:
     import subprocess
     subprocess.Popen(['gepetto-gui'])
 
@@ -478,9 +606,26 @@ class PoseSolver:
 
     def solve(self):
         n_max_iter = 5
+
+        # subprocess.Popen(['gepetto-gui'])
+        # time.sleep(4)
+        self.human.robot.initDisplay(loadModel=True)
+        self.human.robot.display(self.human.robot.q0)
+        self.human.robot.viewer.gui.refresh()
+
         obs_pose_3d = triangulate_point_groups_from_multiple_views_linear(self.cam_projs,
                                                                           self.cam_poses_2d, 0.01, True)
         init_param = PoseShapeParam(joint_placements=None, pose=self.human.robot.q0.copy())
-        param_1 = solve_pose_reprojection(self.human, self.cam_poses_2d, self.cam_calibs,
-                                          obs_pose_3d, self.obs_kps_idxs, self.skel_kps_idxs, init_param, n_max_iter)
-        # param_1 = solve_pose(self.human, obs_pose_3d, self.obs_kps_idxs, self.skel_kps_idxs, init_param, n_max_iter)
+        time.sleep(5)
+        param_1 = solve_pose_from_2d(self.human, self.cam_poses_2d, self.cam_calibs,
+                                     obs_pose_3d, self.obs_kps_idxs, self.skel_kps_idxs, init_param,
+                                     vis=True,
+                                     n_max_iter=100)
+
+        opt_blen = True
+        if opt_blen:
+            init_param = param_1
+            solve_bone_length_from_2d(self.human, self.cam_poses_2d, self.cam_calibs,
+                                      obs_pose_3d, self.obs_kps_idxs, self.skel_kps_idxs, init_param,
+                                      vis=True,
+                                      n_max_iter=10)
